c6da0a0027e6bb5357286082e28979662a5d39f3
Adrian-CCRW <131788073+Adrian-CCRW@users.noreply.github.com>
2023-04-25 13:12:03 -0400

    Delete Install.ps1

--- Install.ps1
+++
@@ -1,8 +0,0 @@
-# Set the path to the configuration.xml file
-$configFilePath = "configuration.xml"
-
-# Run the command to download the configuration file
-Start-Process -FilePath "setup.exe" -ArgumentList "/download", $configFilePath -Wait
-
-# Run the command to configure the application using the downloaded configuration file
-Start-Process -FilePath "setup.exe" -ArgumentList "/configure", $configFilePath -Wait
