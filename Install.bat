b5600a87490bead7f3cbbf072a47f5537138acd0
Adrian-CCRW <131788073+Adrian-CCRW@users.noreply.github.com>
2023-05-05 11:25:45 -0400

    Add files via upload

--- Install.bat
+++ Install.bat
@@ -1,12 +1,3 @@
 @echo off
-
-PowerShell.exe -ExecutionPolicy Bypass -File "Uninstall Office.ps1"
-
-if %ERRORLEVEL% NEQ 0 (
-    echo "Uninstall Office.ps1 failed. Aborting script."
-    pause
-    exit /b %ERRORLEVEL%
-)
-
 PowerShell.exe -ExecutionPolicy Bypass -File "script.ps1"
 pause
